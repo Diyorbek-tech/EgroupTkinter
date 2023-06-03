ec264a8421c1629471e747637b5a5a88baca7888
Diyorbek <67675939+Diyorbek-tech@users.noreply.github.com>
2023-06-03 18:10:08 +0500

    tkinter Option menu codini yozdim

--- Dars7/dars7.py
+++ Dars7/dars7.py
@@ -1,5 +1,41 @@
-print(5+2)
-print("salom")
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.messagebox import *
+
+oyna=Tk()
+oyna.geometry("500x500")
+
+def click(*args):
+    match var.get():
+        case "Uz":showinfo("title","salom")
+        case "Ru":showinfo("title","privet")
+        case "En":showinfo("title","hello")
+
+
+
+var=StringVar()
+menu_elements=[
+    "Tilni tanlang",
+    "Ru",
+    "En",
+    "Kz",
+    "Uz"
+
+]
+
+menu=OptionMenu(oyna,var,*menu_elements,command=click)
+menu.pack()
+
+
+
+
+
+
+
+oyna.mainloop()
+
+
+
 
 
 
